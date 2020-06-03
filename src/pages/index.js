247c552acf29966c61386e3c4424b9dfbdd2b6fc
RJ Salvador <rjsalvadorr@gmail.com>
2020-06-02 22:01:58 -0400

    #1 - added calendar component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {Link} from 'gatsby';
+// import {Link} from 'gatsby';
 import Select from 'react-select';
+import Calendar from 'react-calendar';
+
 
 import Layout from '../components/layout';
-import Image from '../components/image';
+// import Image from '../components/image';
 import SEO from '../components/seo';
 
 const options = [
@@ -18,13 +20,31 @@ const IndexPage = () => (
     <h1>routinetrax</h1>
     <p>Paper-based habits tracking, supercharged</p>
 
-    <Select
-      value={options[0]}
-      onChange={() => {
-        console.log('changed select');
-      }}
-      options={options}
-    />
+    <div class="control-group">
+      <span class="label">Starting month</span>
+      <Calendar className="rt-control rt-control--calendar" maxDetail="year"  minDetail="year" defaultView="year"/>
+    </div>
+    <div class="control-group">
+      <span class="label">Ending month</span>
+      <Calendar className="rt-control rt-control--calendar" maxDetail="year"  minDetail="year" defaultView="year"/>
+    </div>
+    <div class="control-group">
+      <span class="label">Paper type</span>
+      <Select className="rt-control rt-control--select" options={options}/>
+    </div>
+    <div class="control-group">
+      <span class="label">Title</span>
+      <input class="rt-control rt-control--text" type="text" id="title" name="title" required maxlength="20" />
+    </div>
+    <div class="control-group">
+      <span class="label">Icon</span>
+      <Select className="rt-control rt-control--select" options={options}/>
+    </div>
+    <div class="control-group">
+      <span class="label">Habit</span>
+      <input class="rt-control rt-control--text" type="text" id="habit-1" name="habit-1" required maxlength="20" />
+    </div>
+
   </Layout>
 );
 
