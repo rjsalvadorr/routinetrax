734679bd0039074f87114d10fe2689fc6a4e32c0
RJ Salvador <rjsalvadorr@gmail.com>
2020-06-20 19:44:52 -0400

    #5 - code cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import Select from 'react-select';
 import { DateTime } from 'luxon';
+
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import jsPDF from 'jspdf';
-import 'jspdf-autotable';
-import testImg from '../icons/food/icon-bacon.png';
-
-const NUM_TABLES = 6; // must be an even number!
-const NUM_PAIRS = NUM_TABLES / 2;
-
-const ICON_SIZE = 5;
-const TABLE_START_Y = 22;
-const TITLE_FONT_SIZE = 24;
-const TITLE_START_X = 14;
-const TITLE_START_Y = 16;
-const TABLE_MARGIN = 115;
+import { computeMonthRows, computeTables } from '../utils/time-calcs';
+import { generatePdf } from '../utils/pdf-utils';
 
 const options = [
     { value: 'chocolate', label: 'Chocolate' },
@@ -27,39 +17,6 @@ const paperOptions = [
     { value: 'letter', label: 'Letter' },
     { value: 'a4', label: 'A4' },
 ]
-
-const computeMonthRows = (dtObj) => {
-    const yr = dtObj.year;
-    const mon = dtObj.month;
-    const dt = DateTime.fromObject({ year: yr, month: mon });
-
-    const monthRows = [];
-    for (let dy = 1; dy <= dt.daysInMonth; dy++) {
-        const dayStr = DateTime.fromObject({ year: yr, month: mon, day: dy }).toFormat('ccc dd');
-        monthRows.push(dayStr);
-    }
-    return monthRows;
-}
-
-const computeTables = (startMonthObj) => {
-    const start = DateTime.fromObject(startMonthObj);
-    const tables = [];
-
-    tables.push({
-        year: start.year,
-        month: start.monthLong,
-        rows: computeMonthRows(start)
-    });
-    for (let offset = 1; offset < NUM_TABLES; offset++) {
-        const currentDt = start.plus({ months: offset });
-        tables.push({
-            year: currentDt.year,
-            month: currentDt.monthLong,
-            rows: computeMonthRows(currentDt)
-        });
-    }
-    return tables;
-}
 
 class IndexPage extends React.Component {
     constructor(props) {
@@ -116,85 +73,8 @@ class IndexPage extends React.Component {
         });
     }
 
-    getPdfDocument(tables) {
-        const pdfDoc = new jsPDF({
-            orientation: 'p',
-            unit: 'mm',
-            format: 'letter',
-        });
-        pdfDoc.setFontSize(TITLE_FONT_SIZE)
-
-        for (let i = 0; i < NUM_PAIRS; i++) {
-            const pageNumber = pdfDoc.internal.getNumberOfPages();
-            const leftIdx = i * 2;
-            const rightIdx = leftIdx + 1;
-            const sp = '  ';
-
-            const leftTable = this.state.tables[leftIdx];
-            const leftRows = leftTable.rows.map(row => {
-                return [row, sp, sp, sp, sp, sp, sp, sp, sp, sp];
-            });
-            const rightTable = this.state.tables[rightIdx];
-            const rightRows = rightTable.rows.map(row => {
-                return [row, sp, sp, sp, sp, sp, sp, sp, sp, sp];
-            });
-
-            const drawCellHook = function(data) {
-                if (data.section === 'head' && data.column.dataKey !== 0) {
-                    const imgX = data.cell.x + 1.5;
-                    const imgY = data.cell.y + 1.5;
-                    pdfDoc.addImage(testImg, 'PNG', imgX, imgY, ICON_SIZE, ICON_SIZE);
-                }
-            };
-            const headStyle = {
-                halign: 'center',
-                textColor: '#000000',
-                fillColor: '#eeeeee',
-                lineWidth: 0.25
-            };
-            const headRow = [
-                ['Day', sp, sp, sp, sp, sp, sp, sp, sp, sp],
-            ];
-            const colStyle = {
-                0: {
-                    halign: 'right',
-                    fillColor: '#eeeeee',
-                }
-            };
-    
-            pdfDoc.text(`${leftTable.month} ${leftTable.year}`, TITLE_START_X, TITLE_START_Y);
-            pdfDoc.autoTable({
-                head: headRow,
-                body: leftRows,
-                theme: 'grid',
-                headStyles: headStyle,
-                columnStyles: colStyle,
-                startY: TABLE_START_Y,
-                didDrawCell: drawCellHook,
-                margin: { right: TABLE_MARGIN },
-            });
-            pdfDoc.setPage(pageNumber);
-
-            const rightTitleX = TITLE_START_X + TABLE_MARGIN - 14;
-            pdfDoc.text(`${rightTable.month} ${rightTable.year}`, rightTitleX, TITLE_START_Y);
-            pdfDoc.autoTable({
-                head: headRow,
-                body: rightRows,
-                theme: 'grid',
-                headStyles: headStyle,
-                columnStyles: colStyle,
-                startY: TABLE_START_Y,
-                didDrawCell: drawCellHook,
-                margin: { left: TABLE_MARGIN },
-            });
-
-            if (i < NUM_PAIRS - 1) {
-                pdfDoc.addPage('letter', 'p');
-            }
-        }
-
-        const filename = `routinetrax-${Date.now()}.pdf`;
-        pdfDoc.save(filename);
+    getPdfDocument() {
+        generatePdf(this.state.tables);
     }
 
     render() {
